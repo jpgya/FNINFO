3b6774f9c67531ea7079c59429110b86ad7069a4
jpgya <vigyanfv@gmail.com>
2025-08-14 22:52:35 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,11 +2,11 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('app-cache').then((cache) => {
       return cache.addAll([
-        './index.html',
-        './styles.css',
-        './script.js',
+        './v1/index.html',
+        './v1/styles.css',
+        './v1/script.js',
         './a0970552-2c0e-4f72-8761-16c3ba8a4f17.png',
-        './mentindex.html'
+        './index.html'
       ]);
     })
   );
